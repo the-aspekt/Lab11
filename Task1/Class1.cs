fdc71dedd173fd567e3ca27fb5db5de6c62e0720
Pavel <sozonovps@gmail.com>
2023-01-24 22:16:10 +0800

    Добавьте файлы проекта.

---
+++ Task1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal struct LinearEquation
+    {
+        double k;
+        double b;
+
+        public double K { get { return k; } set { } }  
+        public double B { get { return b; } set { } }
+
+        public LinearEquation(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+
+        public void Root()
+        {
+            Console.WriteLine("Решением уравнения {0}x+{1}=0 является число {2}", k, b, -b/k);
+        }
+
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k, b;
+            Console.WriteLine("Введите параметры линейного уравнения типа kx+b=0. \n Данная программа найдет решение этого уравнения");
+            try
+            {
+                Console.WriteLine("Введите параметр k");
+                k = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Введите параметр b");
+                b = Convert.ToDouble(Console.ReadLine());
+                LinearEquation linearEquation= new LinearEquation(k,b);
+                linearEquation.Root();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey();  
+        }
+    }
+}
